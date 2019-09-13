502763c2698d38e4080f3d0796e52b7df59d1884
Josh Hone <joshhone@yahoo.com>
2019-09-13 21:03:13 +0000

    Auto deploy check

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Bacon City!</h2></center>
-    ${PREFIX}'s BACON BACON BACON BACON BACON. <br/> BACON SHARK DOO DOO DOO DOO DOO DOO DOO
+    ${PREFIX}'s BACON BACON BACON BACON BACON. <br/> BACON SHARK DOO DOO DOO DOO DOO DOO DOOP
   <!-- END -->
   
   </div>
